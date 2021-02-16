dd3a24677159e9afd6231c9a5513474393f98c96
Itsuki Watanabe <lifeisencrypted@gmail.com>
2021-02-15 20:02:39 -0800

    a

---
+++ test/index.ts
@@ -0,0 +1,5 @@
+import * as sel_req from '../src/index';
+sel_req.createDriver();
+sel_req.setOrigin('https://example.com:80', 'poop');
+sel_req.request('https://example.com', 'post', { 'Content-Type': 'application/json' }, JSON.stringify({ foo: 'bar' }));
+sel_req.closeDriver();
