9e145b069e792528ee23bcb39bfab5a619d00865
Itsuki Watanabe <lifeisencrypted@gmail.com>
2021-02-15 20:39:46 -0800

    Update index.ts

--- test/index.ts
+++ test/index.ts
@@ -3,3 +3,4 @@ sel_req.createDriver();
 sel_req.setOrigin('https://example.com:80', 'poop');
 sel_req.request('https://example.com', 'post', { 'Content-Type': 'application/json' }, JSON.stringify({ foo: 'bar' }));
 sel_req.closeDriver();
+console.log('uwu');
